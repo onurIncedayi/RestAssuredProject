bbb689fb7b689f24b8685e5b49640a54bed5bcbe
asus <makif_cetin@hotmail.com>
2022-10-14 19:36:56 +0300

    Res12 Nationality

--- src/test/java/Campus/RES_3/DocumentTest.java
+++ src/test/java/Campus/RES_3/DocumentTest.java
@@ -1,9 +1,216 @@
 package Campus.RES_3;
 
 
+import Campus.RES_3.Model.Document;
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookies;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class DocumentTest {
 
 
+    Cookies cookies;
+
+    @BeforeClass
+    public void loginCampus() {
+
+
+        baseURI = "https://demo.mersys.io/";
+
+        Map<String, String> credential = new HashMap<>();
+        credential.put("username", "richfield.edu");
+        credential.put("password", "Richfield2020!");
+        credential.put("rememberMe", "true");
+
+        cookies =
+                given()
+
+                        .contentType(ContentType.JSON)
+                        .body(credential)
+
+
+                        .when()
+                        .post("auth/login")
+
+
+                        .then()
+                        // .log().cookies()
+                        .statusCode(200)
+                        .extract().response().getDetailedCookies()
+
+        ;
+    }
+
+
+    String documentID;
+    String documentName;
+    String documentStage;
+
+
+    String[] attachmentStages;
+
+
+    @Test
+    public void createDocument() {
+        documentName = getRandomName();
+        documentStage = "Student_Registration";
+        attachmentStages = new String[6];
+        attachmentStages[0] = "Student_Registration";
+        Document document = new Document();
+        document.setName(documentName);
+        document.setAttachmentStages(attachmentStages);
+        document.setDescription("document");
+        document.setTranslateName("translateName");
+        document.setSchoolId("schoolId");
+        document.setActive(Boolean.valueOf("active"));
+        document.setRequired(Boolean.valueOf("required"));
+        document.setUseCamera(Boolean.valueOf("usecamera"));
+
+
+        documentID =
+                given()
+
+                        .cookies(cookies)
+                        .contentType(ContentType.JSON)
+                        .body(document)
+
+                        .when()
+                        .post("school-service/api/attachments")
+
+                        .then()
+                        .statusCode(201)
+                        .extract().jsonPath().getString("id")
+
+        ;
+    }
+
+    public String getRandomName() {
+        return RandomStringUtils.randomAlphabetic(6).toLowerCase();
+    }
+
+    @Test(dependsOnMethods = "createDocument")
+    public void createDocumentNegative() {
+
+        Document document = new Document();
+        document.setName(documentName);
+        document.setAttachmentStages(attachmentStages);
+
+
+        given()
+
+                .cookies(cookies)
+                .body(document)
+
+                .when()
+                .post("school-service/api/attachments")
+                .then()
+                .log().body()
+                .statusCode(400)
+
+        ;
+    }
+
+    @Test(dependsOnMethods = "createDocument")
+    public void updateDocument() {
+
+
+        Document document = new Document();
+        document.setId(documentID);
+        document.setName(documentName);
+        document.setAttachmentStages(attachmentStages);
+
+
+        given()
+
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(document)
+
+                .when()
+                .put("school-service/api/attachments")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("name", equalTo(documentName))
+
+
+        ;
+    }
+
+    @Test(dependsOnMethods = "updateDocument")
+    public void deleteDocumentById() {
+        given()
+                .cookies(cookies)
+                .pathParam("documentID", documentID)
+
+                .when()
+                .delete("school-service/api/attachments/{documentID}")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+        ;
+    }
+
+    @Test(dependsOnMethods = "updateDocument")
+    public void deleteDocumentByIdNegative() {
+        given()
+                .cookies(cookies)
+                .pathParam("documentID", documentID)
+                .log().uri()
+                .when()
+                .delete("school-service/api/attachments/{documentID}}")
+
+                .then()
+                .log().body()
+                .statusCode(400)
+        ;
+    }
+
+    @Test(dependsOnMethods = "deleteDocumentById")
+    public void updateNationalityNegative() {
+        documentName = getRandomName();
+        Document document = new Document();
+        document.setId(documentID);
+        document.setName(documentName);
+        document.setAttachmentStages(attachmentStages);
+
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(document)
+
+                .when()
+                .put("school-service/api/attachments")
+
+                .then()
+                .log().body()
+                .statusCode(400)
+
+        ;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- src/test/java/Campus/RES_3/Model/Document.java
+++ src/test/java/Campus/RES_3/Model/Document.java
@@ -1,8 +1,109 @@
 package Campus.RES_3.Model;
 
-
+import java.util.Arrays;
 
 public class Document {
 
 
-}
+        private String name;
+        private String id;
+
+        public String[] attachmentStages;
+
+        private String description;
+        private String translateName;
+        private String schoolId;
+        private Boolean active;
+        private Boolean required;
+        private Boolean useCamera;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String[] getAttachmentStages() {
+            return attachmentStages;
+        }
+
+        public void setAttachmentStages(String[] attachmentStages) {
+            this.attachmentStages = attachmentStages;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getTranslateName() {
+            return translateName;
+        }
+
+        public void setTranslateName(String translateName) {
+            this.translateName = translateName;
+        }
+
+        public String getSchoolId() {
+            return schoolId;
+        }
+
+        public void setSchoolId(String schoolId) {
+            this.schoolId = schoolId;
+        }
+
+        public Boolean getActive() {
+            return active;
+        }
+
+        public void setActive(Boolean active) {
+            this.active = active;
+        }
+
+        public Boolean getRequired() {
+            return required;
+        }
+
+        public void setRequired(Boolean required) {
+            this.required = required;
+        }
+
+        public Boolean getUseCamera() {
+            return useCamera;
+        }
+
+        public void setUseCamera(Boolean useCamera) {
+            this.useCamera = useCamera;
+        }
+
+        @Override
+        public String toString() {
+            return "Document{" +
+                    "name='" + name + '\'' +
+                    ", id='" + id + '\'' +
+                    ", attachmentStages=" + Arrays.toString(attachmentStages) +
+                    ", description='" + description + '\'' +
+                    ", translateName='" + translateName + '\'' +
+                    ", schoolId='" + schoolId + '\'' +
+                    ", active=" + active +
+                    ", required=" + required +
+                    ", useCamera=" + useCamera +
+                    '}';
+        }
+    }
+
+
+
